c4d301181446ed4360650421519256ba26615727
Jack Temple <220726590+fullstack-jack@users.noreply.github.com>
2025-07-21 14:05:05 +0100

    added core variables

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+let firstNum
+let secondNum
+let operand
 
 const add = (num1, num2) => {
     console.log(`${num1 + num2}`)
@@ -12,8 +14,3 @@ const multiply = (num1, num2) => {
 const divide = (num1, num2) => {
     console.log(`${num1 / num2}`)
 }
-
-add(10, 5)
-subtract(10, 5)
-multiply(10, 5)
-divide(10, 5)
