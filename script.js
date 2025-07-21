cb0e4c06aa20ac4b4efdc1397996e4cf344b0701
Jack Temple <220726590+fullstack-jack@users.noreply.github.com>
2025-07-21 14:10:01 +0100

    Created operate function

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ const multiply = (num1, num2) => {
 const divide = (num1, num2) => {
     console.log(`${num1 / num2}`)
 }
+
+const operate = (num1, num2, op) => {
+    
+    switch(op) {
+        case '+':
+            add(num1, num2)
+            break
+        case '-':
+            subtract(num1, num2)
+            break
+        case 'x':
+            multiply(num1, num2)
+            break
+        case '%':
+            divide(num1, num2)
+            break
+    }
+
+}
