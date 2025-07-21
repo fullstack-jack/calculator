6477b03a5aa37bd7d2cc5f8f8b06fb6547202ed8
Jack Temple <220726590+fullstack-jack@users.noreply.github.com>
2025-07-21 14:03:50 +0100

    Added core functions

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+const add = (num1, num2) => {
+    console.log(`${num1 + num2}`)
+}
+const subtract = (num1, num2) => {
+    console.log(`${num1 - num2}`)
+}
+const multiply = (num1, num2) => {
+    console.log(`${num1 * num2}`)
+}
+const divide = (num1, num2) => {
+    console.log(`${num1 / num2}`)
+}
+
+add(10, 5)
+subtract(10, 5)
+multiply(10, 5)
+divide(10, 5)
